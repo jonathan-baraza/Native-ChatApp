c2b8bd3cc82987ff1125ce2643d3e53f5451653a
jonathan_baraza <jbaraza46@gmail.com>
2023-06-24 21:00:39 +0300

    firebase initialization

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-
+import Constants from "expo-constants";
 export default function App() {
   return (
     <View style={styles.container}>
