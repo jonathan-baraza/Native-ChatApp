d39f05fe07ebeb793110c8d643905cf634debe87
jonathan_baraza <jbaraza46@gmail.com>
2023-06-25 14:52:46 +0300

    fix: navigation type error

--- screens/Login.tsx
+++ screens/Login.tsx
@@ -1,13 +1,15 @@
 import { View, Text, Image, TextInput, TouchableOpacity } from "react-native";
 import { useState, useEffect } from "react";
+import { useNavigation, ParamListBase } from "@react-navigation/native";
 
 const Login = () => {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+  const navigation: any = useNavigation();
   return (
-    <View className="flex-1">
+    <View className="flex-1 bg-white">
       <Image
-        className="w-[100%] h-[35vh]"
+        className="w-[100%] h-[40vh]"
         source={require("../assets/backImage.png")}
         resizeMode="cover"
       />
@@ -19,12 +21,16 @@ const Login = () => {
           className="bg-[#f3f5f7] p-3 rounded-xl px-5"
           placeholder="Enter Email"
           keyboardType="email-address"
+          textContentType="emailAddress"
+          autoCapitalize="none"
           value={email}
           onChangeText={setEmail}
         />
         <TextInput
           className="bg-[#f3f5f7] p-3 mt-6 rounded-xl px-5"
           placeholder="Enter Password"
+          textContentType="password"
+          autoCapitalize="none"
           secureTextEntry={true}
           value={password}
           onChangeText={setPassword}
@@ -38,7 +44,13 @@ const Login = () => {
         </TouchableOpacity>
         <View className="flex-row justify-center mt-6">
           <Text>Don't have an account?</Text>
-          <Text className="text-[#ed6d01] ml-2">Sign Up</Text>
+          <TouchableOpacity
+            onPress={() => navigation.navigate("signup")}
+            activeOpacity={0.7}
+            className=" ml-1"
+          >
+            <Text className="text-[#ed6d01]">Sign up</Text>
+          </TouchableOpacity>
         </View>
       </View>
     </View>
