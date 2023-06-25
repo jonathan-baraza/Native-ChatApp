06da5555cbbd701fa9bca880d4721a366a1ac578
jonathan_baraza <jbaraza46@gmail.com>
2023-06-25 12:50:13 +0300

    navigation init

--- App.tsx
+++ App.tsx
@@ -1,11 +1,28 @@
 import React from "react";
 import { View } from "react-native";
 import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Chat from "./screens/Chat";
+
+const Stack = createStackNavigator();
+
+function ChatStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="chat" component={Chat} />
+    </Stack.Navigator>
+  );
+}
+
+function RootNavigator() {
+  return (
+    <NavigationContainer>
+      <ChatStack />
+    </NavigationContainer>
+  );
+}
 
 export default function App() {
-  return (
-    <View>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <RootNavigator />;
 }
