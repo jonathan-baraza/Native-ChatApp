37332534046447ccf0efb6d8b71b9e3fc3d19464
jonathan_baraza <jbaraza46@gmail.com>
2023-06-26 21:32:57 +0300

    headerleft btn

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,12 +1,26 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, StatusBar } from "react-native";
+import { useEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { FontAwesome } from "@expo/vector-icons";
 
 const Home = () => {
+  const navigation = useNavigation();
+  useEffect(() => {
+    navigation.setOptions({
+      headerLeft: () => <FontAwesome name="search" size={24} color="blue" />,
+      headerRight: () => <FontAwesome name="search" size={24} color="blue" />,
+    });
+  }, [navigation]);
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Home</Text>
     </View>
-  )
-}
+  );
+};
 
-export default Home
+const styles = StyleSheet.create({
+  container: {
+    marginTop: StatusBar.currentHeight || 0,
+  },
+});
+export default Home;
