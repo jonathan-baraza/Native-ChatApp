fb9191dcee01a0447d3a446acfc3ca7be68f72c8
jonathan_baraza <jbaraza46@gmail.com>
2023-06-26 21:35:23 +0300

    cat image url

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,14 +1,24 @@
-import { View, Text, StyleSheet, StatusBar } from "react-native";
+import { View, Text, StyleSheet, StatusBar, Image } from "react-native";
 import { useEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { FontAwesome } from "@expo/vector-icons";
-
+const catImageUrl =
+  "https://i.guim.co.uk/img/media/26392d05302e02f7bf4eb143bb84c8097d09144b/446_167_3683_2210/master/3683.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=49ed3252c0b2ffb49cf8b508892e452d";
 const Home = () => {
   const navigation = useNavigation();
   useEffect(() => {
     navigation.setOptions({
       headerLeft: () => <FontAwesome name="search" size={24} color="blue" />,
-      headerRight: () => <FontAwesome name="search" size={24} color="blue" />,
+      headerRight: () => (
+        <Image
+          source={{ uri: catImageUrl }}
+          style={{
+            width: 40,
+            height: 40,
+            marginRight: 15,
+          }}
+        />
+      ),
     });
   }, [navigation]);
   return (
